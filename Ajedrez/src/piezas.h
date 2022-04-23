de2c20bfd3b724042a6a65e15cb263a8025def07
rpolvorinos <61227792+rpolvorinos@users.noreply.github.com>
2022-04-23 16:56:42 +0200

    Add files via upload

---
+++ Ajedrez/src/piezas.h
@@ -0,0 +1,13 @@
+class Piezas
+{
+public:
+	void tecla(unsigned char key);
+	void inicializa();
+	void rotarOjo();
+	void mueve();
+	void dibuja();
+
+	float x_ojo;
+	float y_ojo;
+	float z_ojo;
+};
