5df87a8ffc32468fd0f1b3fc6fc1dbccb8e7fc2a
rpolvorinos <61227792+rpolvorinos@users.noreply.github.com>
2022-05-09 12:46:33 +0200

    Add files via upload

---
+++ Ajedrez/src/Caballo.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "Vector.h"
+#include "ETSIDI.h"
+
+using ETSIDI::Sprite;
+
+class Caballo
+{
+private:
+	Sprite sprite1{ "imagenes/caballo_w.png", 5 };
+	Sprite sprite2{ "imagenes/caballo_b.png", 5 };
+	float radio;
+	int fila, columna;
+	Vector posicion;
+public:
+	Caballo();
+	virtual ~Caballo() {};
+	void dibujaw();
+	void dibujab();
+	void setDatos(float _r, int _f, int _c);
+};
