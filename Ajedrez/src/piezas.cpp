b5524cc64750f18c3b3e74fdd26a4b6c806f8d66
adrismap <61022558+adrismap@users.noreply.github.com>
2022-05-21 12:57:54 +0200

    Update piezas.cpp

--- Ajedrez/src/piezas.cpp
+++ Ajedrez/src/piezas.cpp
@@ -57,4 +57,55 @@ void Piezas::inicializa() {
 		piezas.agregar(aux);
 	}
 
-	
+//datos de incializacion de los caballos
+	for (float i = 2; i < 8; i = i + 5)
+	{
+		Caballo* aux = new Caballo(i, 8);
+		piezas.agregar(aux);
+	}
+
+	for (float i = 2; i < 8; i = i + 5)
+	{
+		Caballo* aux = new Caballo(i, 1);
+		piezas.agregar(aux);
+	}
+
+	estado = 0;
+}
+
+void Piezas::dibuja() {
+
+	piezas.dibuja();
+}
+
+void Piezas::tecla(unsigned char key) {
+
+	/**switch (key) {
+
+	case GLUT_KEY_DOWN:
+		torre1.getFC(torre1.fc.fila, torre1.fc.columna);
+		torre1.fc = torre1.vertical(-1);
+		break;
+
+	case GLUT_KEY_UP:
+		torre1.getFC(torre1.fc.fila, torre1.fc.columna);
+		torre1.fc = torre1.vertical(1);
+		break;
+
+	case GLUT_KEY_LEFT:
+		torre1.getFC(torre1.fc.fila, torre1.fc.columna);
+		torre1.fc = torre1.horizontal(-1);
+		break;
+
+	case GLUT_KEY_RIGHT:
+		torre1.getFC(torre1.fc.fila, torre1.fc.columna);
+		torre1.fc = torre1.horizontal(1);
+		break;
+	}*/
+
+}
+
+void Piezas::mueve() {
+
+	/*torre1.movimiento(torre1.fc.fila, torre1.fc.columna);*/
+}
