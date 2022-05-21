c5cec7dae83073145a3682c7596e3d6b2519573b
adrismap <61022558+adrismap@users.noreply.github.com>
2022-05-21 12:55:38 +0200

    Update Interaccion.cpp

--- Ajedrez/src/Interaccion.cpp
+++ Ajedrez/src/Interaccion.cpp
@@ -106,3 +106,54 @@ void Interaccion::mov(Caballo& t, Selector s, int& _turno)
 	}
 }
 
+void Interaccion::mov(Rey& t, Selector s, int& _turno)
+{
+	if (t.estado == 0 && t.fc.fila == s.fila && t.fc.columna == s.columna && t.color == _turno)
+	{
+		t.estado++;
+	}
+	else
+	{
+		if (t.estado == 1 && (abs(s.columna - t.fc.columna) <= 1 && abs(s.fila - t.fc.fila) <= 1) && t.color == _turno)
+		{
+			t.setDatos(0.5, s.getFila(), s.getColumna());
+			t.estado--;
+
+			switch (_turno)
+			{
+			case 0:
+				_turno = 1;
+				break;
+			case 1:
+				_turno = 0;
+				break;
+			}
+		}
+	}
+}
+
+void Interaccion::mov(Dama& t, Selector s, int& _turno)
+{
+	if (t.estado == 0 && t.fc.fila == s.fila && t.fc.columna == s.columna && t.color == _turno)
+	{
+		t.estado++;
+	}
+	else
+	{
+		if (t.estado == 1 && (s.fila == t.fc.fila || s.columna == t.fc.columna|| (abs(s.fila - t.fc.fila) == abs(s.columna - t.fc.columna))) && t.color == _turno)
+		{
+			t.setDatos(0.5, s.getFila(), s.getColumna());
+			t.estado--;
+
+			switch (_turno)
+			{
+			case 0:
+				_turno = 1;
+				break;
+			case 1:
+				_turno = 0;
+				break;
+			}
+		}
+	}
+}
