3ce91e6d3150f7e0c20db24c2cc5799a452670f4
adrismap <61022558+adrismap@users.noreply.github.com>
2022-05-21 12:56:39 +0200

    Update Selector.cpp

--- Ajedrez/src/Selector.cpp
+++ Ajedrez/src/Selector.cpp
@@ -10,33 +10,3 @@ void Selector::inicializa() {
 	Selector selector;
 }
 
-void Selector::mover(unsigned char key) {
-	switch (key)
-	{
-	case GLUT_KEY_LEFT:
-		if (fila > 1)
-			fila--;
-		break;
-	case GLUT_KEY_DOWN:
-		if (columna > 1)
-			columna--;
-		break;
-	case GLUT_KEY_RIGHT:
-		if (fila < 8)
-			fila++;
-		break;
-	case GLUT_KEY_UP:
-		if (columna < 8)
-			columna++;
-		break;
-	}
-}
-int Selector::getFila()
-{
-	return fila;
-}
-
-int Selector::getColumna()
-{
-	return columna;
-}
