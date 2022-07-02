45fc20335b42b225952d02f9c1348277490ff6bd
rpolvorinos <61227792+rpolvorinos@users.noreply.github.com>
2022-07-02 21:03:37 +0200

    Add files via upload

--- Ajedrez_v2/src/Interaccion.cpp
+++ Ajedrez_v2/src/Interaccion.cpp
@@ -1,45 +1,8 @@
 #include "Interaccion.h"
 #include "math.h"
 
-void Interaccion::moverTorre(Pieza& t, Selector s, int& _turno, int& _o, ListasPiezas& l)
-{
-	if (t.estado == 0 && t.fc.getF() == s.getFila() && t.fc.getC() == s.getColumna()&& t.color == _turno)
-	{
-		t.estado++;
-		_o = 2;
-	}
-	else
-	{
-		if (t.estado == 1 && (s.getFila()== t.fc.getF() || s.getFila() == t.fc.getC()) && t.color == _turno && _turno != _o)
-		{
-			Pieza* aux = l.colision(s.getFila(), s.getColumna(), _turno);
-			if (aux != 0 && _turno != aux->color)
-			{
-				l.eliminar(aux);
-			}
-			if (aux != 0 && _turno == aux->color) {}
-			else {
 
-				t.setDatos(0.5, s.getFila(), s.getColumna());
-				t.estado--;
-
-				_o = _turno;
-
-				switch (_turno)
-				{
-				case 0:
-					_turno = 1;
-					break;
-				case 1:
-					_turno = 0;
-					break;
-				}
-			}
-		}
-	}
-}
-
-void Interaccion::moverAlfil(Pieza& t, Selector s, int& _turno, int& _o, ListasPiezas& l)
+void Interaccion::moverPieza(Pieza& t, Selector s, int& _turno, int& _o, ListasPiezas& l)
 {
 	if (t.estado == 0 && t.fc.getF() == s.getFila() && t.fc.getC() == s.getColumna() && t.color == _turno)
 	{
@@ -48,7 +11,8 @@ void Interaccion::moverAlfil(Pieza& t, Selector s, int& _turno, int& _o, ListasP
 	}
 	else
 	{
-		if (t.estado == 1 && (abs(s.getFila()-t.fc.getF()) == abs(s.getColumna()-t.fc.getC())) && t.color == _turno && _turno != _o)
+		//if (t.estado == 1 && (s.getFila() == t.fc.getF() || s.getColumna() == t.fc.getC()|| (abs(s.getFila() - t.fc.getF()) == abs(s.getColumna() - t.fc.getC()))) && t.color == _turno && _turno != _o)
+		if(condicion(t,s,_turno,_o,l))
 		{
 			Pieza* aux = l.colision(s.getFila(), s.getColumna(), _turno);
 			if (aux != 0 && _turno != aux->color)
@@ -70,178 +34,49 @@ void Interaccion::moverAlfil(Pieza& t, Selector s, int& _turno, int& _o, ListasP
 					break;
 				}
 			}
-			
 		}
 	}
 }
 
-void Interaccion::moverPeon(Pieza& t, Selector s, int& _turno, int& _o, ListasPiezas& l)
+bool Interaccion::condicion(Pieza& t, Selector s, int& _turno, int& _o, ListasPiezas& l)
 {
-	if (t.estado == 0 && t.fc.getF() == s.getFila() && t.fc.getC() == s.getColumna() && t.color == _turno)
+	switch (t.getTipoPieza())
 	{
-		t.estado++;
-		_o = 2;
-	}
-	else
-	{
-		if(t.color == 0 && t.estado == 1 && ((s.getColumna() - t.fc.getC()) == 1) && ((s.getFila() - t.fc.getF()) == 0) && t.color == _turno && _turno != _o) {
-			Pieza* aux = l.colision(s.getFila(), s.getColumna(), _turno);
-			if (aux != 0 && _turno != aux->color)
-			{
-				l.eliminar(aux);
-			}
-			if (aux != 0 && _turno == aux->color) {}
-			else {
-				t.setDatos(0.5, s.getFila(), s.getColumna());
-				t.estado--;
-				_o = _turno;
-				switch (_turno)
-				{
-				case 0:
-					_turno = 1;
-					break;
-				case 1:
-					_turno = 0;
-					break;
-				}
-			}
+	case 1:
+		if (t.estado == 1 && (s.getFila() == t.fc.getF() || s.getFila() == t.fc.getC()) && t.color == _turno && _turno != _o)
+			return true;
+		break;
 
-		}
-		if (t.color == 1 && t.estado == 1 && ((s.getColumna() - t.fc.getC())==-1) && ((s.getFila() - t.fc.getF())==0) && t.color == _turno && _turno != _o)
-		{
-			Pieza* aux = l.colision(s.getFila(), s.getColumna(), _turno);
-			if (aux != 0 && _turno != aux->color)
-			{
-				l.eliminar(aux);
-			}
-			if (aux != 0 && _turno == aux->color) {}
-			else {
-				t.setDatos(0.5, s.getFila(), s.getColumna());
-				t.estado--;
-				_o = _turno;
-				switch (_turno)
-				{
-				case 0:
-					_turno = 1;
-					break;
-				case 1:
-					_turno = 0;
-					break;
-				}
-			}
-			
-		}
-	}
-}
+	case 2:
+		if (t.estado == 1 && ((abs(s.getColumna() - t.fc.getC()) == 1 && abs(s.getFila() - t.fc.getF()) == 2) || (abs(s.getColumna() - t.fc.getC()) == 2 && abs(s.getFila() - t.fc.getF()) == 1)) && t.color == _turno && _turno != _o)
+		return true;
+		break;
 
-void Interaccion::moverCaballo(Pieza& t, Selector s, int& _turno, int& _o, ListasPiezas& l)
-{
-	if (t.estado == 0 && t.fc.getF() == s.getFila() && t.fc.getC() == s.getColumna() && t.color == _turno)
-	{
-		t.estado++;
-		_o = 2;
-	}
-	else
-	{
-		if (t.estado == 1 && ((abs(s.getColumna() - t.fc.getC()) == 1&& abs(s.getFila() - t.fc.getF()) == 2)|| (abs(s.getColumna() - t.fc.getC()) == 2 && abs(s.getFila() - t.fc.getF()) == 1)) && t.color == _turno && _turno != _o)
-		{
-			Pieza* aux = l.colision(s.getFila(), s.getColumna(), _turno);
-			if (aux != 0 && _turno != aux->color)
-			{
-				l.eliminar(aux);
-			}
+	case 3:
+		if (t.estado == 1 && (abs(s.getFila() - t.fc.getF()) == abs(s.getColumna() - t.fc.getC())) && t.color == _turno && _turno != _o)
+		return true;
+		break;
 
-			if (aux!=0 && _turno == aux->color){}
-			else {
-				t.setDatos(0.5, s.getFila(), s.getColumna());
-				t.estado--;
-				_o = _turno;
-				switch (_turno)
-				{
-				case 0:
-					_turno = 1;
-					break;
-				case 1:
-					_turno = 0;
-					break;
-				}
-			}
-			
-		}
-	}
-}
-
-void Interaccion::moverRey(Pieza& t, Selector s, int& _turno, int& _o, ListasPiezas& l)
-{
-	if (t.estado == 0 && t.fc.getF() == s.getFila() && t.fc.getC() == s.getColumna() && t.color == _turno)
-	{
-		t.estado++;
-		_o = 2;
-	}
-	else
-	{
-		if (t.estado == 1 && (abs(s.getColumna() - t.fc.getC()) <= 1 && abs(s.getFila() - t.fc.getF()) <= 1) && t.color == _turno && _turno != _o)
-		{
-			Pieza* aux = l.colision(s.getFila(), s.getColumna(), _turno);
-			if (aux != 0 && _turno != aux->color)
-			{
-				l.eliminar(aux);
-			}
-			if (aux != 0 && _turno == aux->color) {}
-			else {
-				t.setDatos(0.5, s.getFila(), s.getColumna());
-				t.estado--;
-				_o = _turno;
-				switch (_turno)
-				{
-				case 0:
-					_turno = 1;
-					break;
-				case 1:
-					_turno = 0;
-					break;
-				}
-
-			}
-		}
-	}
-}
-
-void Interaccion::moverDama(Pieza& t, Selector s, int& _turno, int& _o, ListasPiezas& l)
-{
-	if (t.estado == 0 && t.fc.getF() == s.getFila() && t.fc.getC() == s.getColumna() && t.color == _turno)
-	{
-		t.estado++;
-		_o = 2;
-	}
-	else
-	{
+	case 4:
 		if (t.estado == 1 && (s.getFila() == t.fc.getF() || s.getColumna() == t.fc.getC()|| (abs(s.getFila() - t.fc.getF()) == abs(s.getColumna() - t.fc.getC()))) && t.color == _turno && _turno != _o)
-		{
-			Pieza* aux = l.colision(s.getFila(), s.getColumna(), _turno);
-			if (aux != 0 && _turno != aux->color)
-			{
-				l.eliminar(aux);
-			}
-			if (aux != 0 && _turno == aux->color) {}
-			else {
-				t.setDatos(0.5, s.getFila(), s.getColumna());
-				t.estado--;
-				_o = _turno;
-				switch (_turno)
-				{
-				case 0:
-					_turno = 1;
-					break;
-				case 1:
-					_turno = 0;
-					break;
-				}
-			}
-		}
+		return true;
+		break;
+
+	case 5:
+		if (t.estado == 1 && (abs(s.getColumna() - t.fc.getC()) <= 1 && abs(s.getFila() - t.fc.getF()) <= 1) && t.color == _turno && _turno != _o)
+		return true;
+		break;
+	
+	case 6:
+		if (t.color == 0 && t.estado == 1 && ((s.getColumna() - t.fc.getC()) == 1) && ((s.getFila() - t.fc.getF()) == 0) && t.color == _turno && _turno != _o) 
+		return true;
+		break;
 	}
+
+	return false;
 }
 
+/*
 void Interaccion::moverPieza(Pieza& t, Selector s, int& _turno, int& _o, ListasPiezas& l) {
 
 	int tipoPieza;
@@ -273,7 +108,7 @@ void Interaccion::moverPieza(Pieza& t, Selector s, int& _turno, int& _o, ListasP
 			break;
 		}
 	}
-}
+}*/
 
 bool Interaccion::captura(Pieza t, int _f, int _c)
 {
